2da4eb73bbe8cfd16a2e645ab1875e7acca74ec6
taeganwarren <warretaega@gmail.com>
2021-03-25 22:31:21 -0700

    Fixed indentation in some files

--- backend/src/resolvers/Link.js
+++ backend/src/resolvers/Link.js
@@ -1,12 +1,12 @@
 module.exports = {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-    postedBy: (parent, args, context, info) => {
-        return context.prisma.link.findUnique({ where: { id: parent.id } }).postedBy();
-    },
-    votes: (parent, args, context, info) => {
-        return context.prisma.link.findUnique({ where: { id: parent.id } }).votes();
-    },
-    createdAt: (parent) => parent.createdAt
+	id: (parent) => parent.id,
+	description: (parent) => parent.description,
+	url: (parent) => parent.url,
+	postedBy: (parent, args, context, info) => {
+		return context.prisma.link.findUnique({ where: { id: parent.id } }).postedBy();
+	},
+	votes: (parent, args, context, info) => {
+		return context.prisma.link.findUnique({ where: { id: parent.id } }).votes();
+	},
+	createdAt: (parent) => parent.createdAt
 };
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -2,27 +2,27 @@ const jwt = require('jsonwebtoken');
 const APP_SECRET = 'GraphQL-is-aw3some';
 
 function getTokenPayload(token) {
-    return jwt.verify(token, APP_SECRET);
+	return jwt.verify(token, APP_SECRET);
 }
 
 function getUserId(req, authToken) {
-    if (req && req.headers.authorization) {
-        const authHeader = req.headers.authorization;
-        const token = authHeader.replace('Bearer ', '');
-        if (!token) {
-            throw new Error('No token found');
-        }
-        const { userId } = getTokenPayload(token);
-        return userId;
-    } else if (authToken) {
-        const { userId } = getTokenPayload(authToken);
-        return userId;
-    } else {
-        throw new Error('Not authenticated');
-    }
+	if (req && req.headers.authorization) {
+		const authHeader = req.headers.authorization;
+		const token = authHeader.replace('Bearer ', '');
+		if (!token) {
+			throw new Error('No token found');
+		}
+		const { userId } = getTokenPayload(token);
+		return userId;
+	} else if (authToken) {
+		const { userId } = getTokenPayload(authToken);
+		return userId;
+	} else {
+		throw new Error('Not authenticated');
+	}
 }
 
 module.exports = {
-    APP_SECRET,
-    getUserId
+	APP_SECRET,
+	getUserId
 };
